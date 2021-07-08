39a2ee544cb1d7639f0fc157fa1eb1ed4e24f172
Murat Çakıroğlu <mcakiroglu@Murat-MBP.home>
2021-07-09 00:07:01 +0300

    day4

--- ReactSpring_FullStack/Spring_Backend/src/main/java/controller/KisiController.java
+++ ReactSpring_FullStack/Spring_Backend/src/main/java/controller/KisiController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -12,12 +13,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import model.Kisi;
 import service.KisiService;
 
 @RestController
+@RequestMapping("/kisiler")
+@CrossOrigin(origins = "http://localhost:8081")
 public class KisiController {
 	
 	public KisiService kisiService;
@@ -27,31 +31,31 @@ public class KisiController {
 		this.kisiService = kisiService;
 	}
 	
-	@GetMapping(path="/kisiler")
+	@GetMapping
 	public List <Kisi> kisileriGetir() {
 		return kisiService.tumKisileriGetir();
 	}
-	@PostMapping(path="/kisiler/ekle")
+	@PostMapping(path="/ekle")
 	public Kisi yeniKisiEkle(@RequestBody Kisi kisi) {
 		return kisiService.kisiEkle(kisi);
 	}
 	
-	@GetMapping(path="/kisiler/{id}")
+	@GetMapping(path="/{id}")
 	public Optional<Kisi> idIleKisiListele(@PathVariable Integer id) {
 		return kisiService.idIleKisiGetir(id);
 	}
 	
-	@DeleteMapping(path="/kisiler/sil/{id}")
+	@DeleteMapping(path="/sil/{id}")
 	public String idIleKisiSil(@PathVariable Integer id) {
 		return kisiService.idIleKisiSil(id);
 	}
 	
-	@PutMapping(path="kisiler/guncelle" )
+	@PutMapping(path="/guncelle")
 	public Kisi guncelle(@RequestBody Kisi yeniKisi) {
 		return kisiService.idIleKisiGuncelle(yeniKisi);
 	}
 	
-	@PatchMapping(path = "/kisiler/yenile/{id}")
+	@PatchMapping(path = "/yenile/{id}")
 	public Kisi idIleKismiGuncelle(@PathVariable Integer id, @Validated @RequestBody Kisi yeniKisi) {
 		return kisiService.idIleKismiGuncelle(id, yeniKisi);
 	}
